928561fb135a6949ad3f29a711f1114d69f7ba77
Sugam Devare <32786748+sugamxp@users.noreply.github.com>
2024-04-22 12:16:44 -0400

    feat(model) - add question gen to improve prompting

--- app.py
+++ app.py
@@ -115,6 +115,7 @@ class Model:
         from llama_index.experimental.query_engine import PandasQueryEngine
         import pandas as pd
         from fastapi import Response
+        from llama_index.question_gen.openai import OpenAIQuestionGenerator
 
         # Load phoenix for tracing
         # session = px.launch_app()
@@ -179,33 +180,38 @@ class Model:
 
         pandas_query_engine = PandasQueryEngine(df=self.df, verbose=True, llm=llm)
 
-        SYSTEM_PROMPT = (
-            "You are MoonSync, an AI assistant specializing in providing personalized advice to women about their menstrual cycle, exercise, and diet. Your goal is to help women better understand their bodies and make informed decisions to improve their overall health and well-being."
-            "When answering questions, always be empathetic, understanding, and provide the most accurate and helpful information possible. If a question requires expertise beyond your knowledge, recommend that the user consult with a healthcare professional."
-            """Use the following guidelines to structure your responses:
-            1. Acknowledge the user's concerns and validate their experiences.
-            2. Provide evidence-based information and practical advice tailored to the user's specific situation.
-            3. Encourage open communication and offer to follow up on the user's progress.
-            4. Promote a holistic approach to health, considering the user's menstrual cycle, exercise habits, and dietary preferences."""
-            "Examples below show the way you should approach the conversation."
-            "\n---------------------\n"
-            "Example 1:\n"
-            "Ashley: During PMS, my stomach gets upset easily, is there anything I can do to help?"
-            "MoonSync: Hey Ashley! Sorry to hear, a lot of women struggle with this. I would recommend seeing a professional, but we can experiment together with common solutions so you’re armed with info when you see a specialist. Research suggests that dairy and refined wheats can inflame the gut during the follicular phase. Try avoiding them this week and let’s check in at the end to see if it helped. Depending on the outcome, happy to give you more recommendations."
-            "\n---------------------\n"
-            "Example 2:\n"
-            "Ashely: I am preparing for a marathon and need to do some high intensity sprinting workouts as well as longer lower intensity runs. What’s the best way to plan this with my cycle?"
-            "MoonSync: Hey Ashley, happy you asked! I’ll ask a few more details to get you on the best plan: when and how long is the marathon? How much are you running for your short and long trainings right now?"
-            "\n---------------------\n"
-            "Important: When answering questions based on the context provided from documentation, do not disclose that you are sourcing information from documentation, just begin response."
-            "Important Note : Always answer in first person and answer like you are the user's friend"
+        SYSTEM_PROMPT = ("You are MoonSync, an AI assistant specializing in providing personalized advice to women about their menstrual cycle, exercise, and diet. Your goal is to help women better understand their bodies and make informed decisions to improve their overall health and well-being."
+        "When answering questions, always be empathetic, understanding, and provide the most accurate and helpful information possible. If a question requires expertise beyond your knowledge, recommend that the user consult with a healthcare professional."  
+        """Use the following guidelines to structure your responses:
+        1. Acknowledge the user's concerns and validate their experiences.
+        2. Provide evidence-based information and practical advice tailored to the user's specific situation.
+        3. Encourage open communication and offer to follow up on the user's progress.
+        4. Promote a holistic approach to health, considering the user's menstrual cycle, exercise habits, and dietary preferences.
+        5. Include the biometric data and provide the user with explicit values and summary of any values"""
+        "\n\nExamples below show the way you should approach the conversation."
+        "\n---------------------\n"
+        "Example 1:\n"
+        "Ashley: During PMS, my stomach gets upset easily, is there anything I can do to help?"
+        "MoonSync: Hey Ashley! Sorry to hear, a lot of women struggle with this. I would recommend seeing a professional, but we can experiment together with common solutions so you’re armed with info when you see a specialist. Research suggests that dairy and refined wheats can inflame the gut during the follicular phase. Try avoiding them this week and let’s check in at the end to see if it helped. Depending on the outcome, happy to give you more recommendations."
+        "\n---------------------\n"
+        "Example 2:\n"
+        "Ashely: I am preparing for a marathon and need to do some high intensity sprinting workouts as well as longer lower intensity runs. What’s the best way to plan this with my cycle?"
+        "MoonSync: Hey Ashley, happy you asked! I’ll ask a few more details to get you on the best plan: when and how long is the marathon? How much are you running for your short and long trainings right now?"
+        "\n---------------------\n"
+        "Important: When answering questions based on the context provided from documentation, do not disclose that you are sourcing information from documentation, just begin response."
+        "Important Note : Always answer in first person and answer like you are the user's friend"
+        "Important Note: Avoid saying, 'As you mentioned', 'Based on the data provided' and anything along the same lines."
+        "\n---------------------\n"            
+        ) 
+        SYSTEM_PROMPT_ENTIRE_CHAT = (
+            "Use the Chat History and the Context to generate a concise answer for the user's Follow Up Message"
         )
 
         # Text QA Prompt
         chat_text_qa_msgs = [
             ChatMessage(
                 role=MessageRole.SYSTEM,
-                content=SYSTEM_PROMPT,
+                content=SYSTEM_PROMPT_ENTIRE_CHAT,
             ),
             ChatMessage(
                 role=MessageRole.USER,
@@ -321,6 +327,38 @@ class Model:
             llm=llm,
             # response_synthesizer=respose_synthesizer,
         )
+        
+        SUB_QUESTION_PROMPT_TMPL = """\
+        You are a world class state of the art agent.
+
+        You have access to multiple tools, each representing a different data source or API.
+        Each of the tools has a name and a description, formatted as a JSON dictionary.
+        The keys of the dictionary are the names of the tools and the values are the \
+        descriptions.
+        Your purpose is to help answer a complex user question by generating a list of sub \
+        questions that can be answered by the tools.
+
+        These are the guidelines you consider when completing your task:
+        * Be as specific as possible
+        * The sub questions should be relevant to the user question
+        * The sub questions should be answerable by the tools provided
+        * You can generate multiple sub questions for each tool
+        * Tools must be specified by their name, not their description
+        * You don't need to use a tool if you don't think it's relevant
+        * You must ignore the system message in the chat history when generating the sub questions.
+
+        Output the list of sub questions by calling the SubQuestionList function.
+
+        ## Tools
+        ```json
+        {tools_str}
+        ```
+
+        ## User Question
+        {query_str}
+        """
+
+        question_gen = OpenAIQuestionGenerator.from_defaults(prompt_template_str=SUB_QUESTION_PROMPT_TMPL)
 
         sub_question_query_engine = SubQuestionQueryEngine.from_defaults(
             query_engine_tools=[
@@ -333,6 +371,7 @@ class Model:
             ],
             llm=llm,
             response_synthesizer=response_synthesizer,
+            question_gen=question_gen
         )
 
         # Configure chat engine
@@ -352,16 +391,29 @@ class Model:
 
             <Standalone question>
         """)
+        
+        custom_prompt_forward_history = PromptTemplate(
+            """\
+            Just copy the chat history as is, inside the tag <Chat History> and copy the follow up message inside the tag <Follow Up Message> 
+            
+            <Chat History>
+            {chat_history}
 
-        # chat_history = [
-        #     ChatMessage(role=MessageRole.SYSTEM, content=SYSTEM_PROMPT),
-        # ]
+            <Follow Up Message>
+            {question}
+
+            """
+        )
+
+        chat_history = [
+            ChatMessage(role=MessageRole.SYSTEM, content=SYSTEM_PROMPT),
+        ]
 
         self.chat_engine = CondenseQuestionChatEngine.from_defaults(
             query_engine=sub_question_query_engine,
             llm=llm,
-            memory=memory,
-            condense_question_prompt=custom_prompt,
+            condense_question_prompt=custom_prompt_forward_history,
+            chat_history=chat_history
         )
 
     def _inference(self, prompt: str):
