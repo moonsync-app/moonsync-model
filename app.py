759868dc13efb9ea3ed2137413d07062bcc84fa6
Rui Chen <rui@chenrui.dev>
2024-04-26 20:45:01 -0400

    refactor: move some prompts into prompts.py
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- app.py
+++ app.py
@@ -22,6 +22,13 @@ from config.base import (
     PPLX_MODEL_TEMPERATURE,
 )
 
+from config.prompts import (
+    SOURCE_QA_PROMPT_SYSTEM,
+    SOURCE_QA_PROMPT_USER,
+    SYSTEM_PROMPT,
+    SYSTEM_PROMPT_ENTIRE_CHAT,
+)
+
 moonsync_image = (
     Image.debian_slim(python_version="3.10")
     .apt_install(
@@ -147,31 +154,11 @@ class Model:
         sources_qa_prompt = [
             ChatMessage(
                 role=MessageRole.SYSTEM,
-                content=(
-                    "You are an expert Q&A system that is trusted around the world.\n"
-                    "Always answer the query using the provided context information with sources, "
-                    "and not prior knowledge.\n"
-                    "Some rules to follow:\n"
-                    "1. IMPORTANT - Include the list of sources of the context in the end of your final answer if you are using that information\n"
-                    "2. Never directly reference the given context in your answer.\n"
-                    "3. Avoid statements like 'Based on the context, ...' or "
-                    "'The context information ...' or anything along "
-                    "those lines."
-                ),
+                content=(SOURCE_QA_PROMPT_SYSTEM),
             ),
             ChatMessage(
                 role=MessageRole.USER,
-                content=(
-                    "Context information is below.\n"
-                    "---------------------\n"
-                    "{context_str}\n"
-                    "---------------------\n"
-                    "Given the context information and not prior knowledge, "
-                    "answer the query.\n"
-                    "IMPORTANT - Include the list of sources of the context in the end of your final answer if you are using that information\n"
-                    "Query: {query_str}\n"
-                    "Answer: "
-                ),
+                content=(SOURCE_QA_PROMPT_USER),
             ),
         ]
         sources_prompt = ChatPromptTemplate(sources_qa_prompt)
@@ -276,38 +263,7 @@ class Model:
         #     retriever=empty_index_retriever, response_synthesizer=synthesizer, pplx=pplx_llm, qa_prompt=pplx_prompt
         # )
 
-        self.SYSTEM_PROMPT = (
-            "You are MoonSync, an AI assistant specializing in providing personalized advice to women about their menstrual cycle, exercise, feelings, and diet. Your goal is to help women better understand their bodies and make informed decisions to improve their overall health and well-being."
-            "When answering questions, always be empathetic, understanding, and provide the most accurate and helpful information possible. If a question requires expertise beyond your knowledge, recommend that the user consult with a healthcare professional."
-            """\nUse the following guidelines to structure your responses:
-        1. Acknowledge the user's concerns and validate their experiences.
-        2. Provide evidence-based information and practical advice tailored to the user's specific situation.
-        3. Encourage open communication and offer to follow up on the user's progress.
-        4. Ask follow up questions to get more information from the user.
-        5. Include the biometric data and provide the user with explicit values and summary of any values
-        6. Answer the query in a natural, friendly, encouraging and human-like manner.
-        7. When answering questions based on the context provided, do not disclose that you are refering to context, just begin response.
-        8. IMPORTANT - Include the list of sources of the context in the end of your final answer if you are using that information\n"""
-            "\n\nExamples below show the way you should approach the conversation."
-            "\n---------------------\n"
-            "Example 1:\n"
-            "Ashley: During PMS, my stomach gets upset easily, is there anything I can do to help?"
-            "MoonSync: Hey Ashley! Sorry to hear, a lot of women struggle with this. I would recommend seeing a professional, but we can experiment together with common solutions so you’re armed with info when you see a specialist. Research suggests that dairy and refined wheats can inflame the gut during the follicular phase. Try avoiding them this week and let’s check in at the end to see if it helped. Depending on the outcome, happy to give you more recommendations."
-            "\n---------------------\n"
-            "Example 2:\n"
-            "Ashely: I am preparing for a marathon and need to do some high intensity sprinting workouts as well as longer lower intensity runs. What’s the best way to plan this with my cycle?"
-            "MoonSync: Hey Ashley, happy you asked! I’ll ask a few more details to get you on the best plan: when and how long is the marathon? How much are you running for your short and long trainings right now?\n\n"
-        )
-        SYSTEM_PROMPT_ENTIRE_CHAT = (
-            "Remember you are MoonSync. Use the Chat History and the Context to generate a detailed answer for the user's Follow Up Message.\n"
-            "Important guidelines you need to follow:\n"
-            "You are given the current menstrual phase, date, and location in the context. Use this information if relevant to the user's message\n"
-            "Include the list of sources of the context in the end of your final answer if you are using that information\n"
-            "Avoid saying, 'As you mentioned', 'Based on the data provided' and anything along the same lines.\n"
-            "Provide specific information and advice based on the context and user's message.\n"
-            "If the users asks for a date or time, provide the exact dates and days and ask the user if she want to schedule the event in the end of your answer.\n"
-            ""
-        )
+        self.SYSTEM_PROMPT = SYSTEM_PROMPT
 
         # Text QA Prompt
         chat_text_qa_msgs = [
