885f5204db78280e4fcd7a4863acc1d8e002c6fe
Sugam Devare <32786748+sugamxp@users.noreply.github.com>
2024-05-07 14:35:15 -0400

    feat - update online models

--- config/base.py
+++ config/base.py
@@ -6,7 +6,7 @@ MODAL_CONTAINER_IDLE_TIMEOUT = 240
 OPENAI_MODEL = "gpt-4-turbo"
 OPENAI_MODEL_TEMPERATURE = 0.1
 
-PPLX_MODEL = "sonar-small-online"
+PPLX_MODEL = "llama-3-sonar-small-32k-online"
 PPLX_MODEL_TEMPERATURE = 0.5
 
 WEATHER_API_URL = "http://api.weatherapi.com/v1/current.json"
